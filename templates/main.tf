533c1914686a928dc16e69bfb3a97e1cfbc5c469
Adrian Ruiz <adrianruizmora@hotmail.com>
2023-05-24 20:15:59 +0200

    feat: commenting local aws authentication

--- templates/main.tf
+++ templates/main.tf
@@ -1,3 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.67.0"
+    }
+  }
+}
+
+
+// Uncomment to Run locally
+// provider "aws" {
+//   shared_config_files      = [""]
+//   shared_credentials_files = [""]
+//   profile                  = "default"
+// }
+
 module "tf-backend" {
   source              = "git::git@github.com:adrianruizmora/infra-delivery.git?ref=tf-backend"
   backend_bucket_name = var.backend_bucket_name
