ddb91457ee8fc402bdb0aaad49f25bfe0d52701b
Adrian Ruiz <adrianruizmora@hotmail.com>
2023-05-25 12:19:58 +0200

    feature: reorganizing templates structure

---
+++ templates/infra/terraform/environment/set_tf_vars.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export TF_VAR_backend_bucket_name=""
+export TF_VAR_aws_region=""
+export TF_VAR_application=""
+export TF_VAR_environment=""
+export TF_VAR_solution_stack=""
+export TF_VAR_ssh_access_key=""
+export TF_VAR_healthcheck_endpoint=""
+export TF_VAR_application_variables='{"key1": "value1", "key2": "value2"}'
