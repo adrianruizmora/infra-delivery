a1f3074241fcbb7eef2cd5c8bb77184784f11916
Adrian Ruiz <adrianruizmora@hotmail.com>
2023-05-26 12:09:51 +0200

    fix: reverting comment

--- main.tf
+++ main.tf
@@ -1,39 +1,39 @@
-# resource "aws_s3_bucket" "terraform_states_backend" {
-#   bucket = var.backend_bucket_name
-#   tags = {
-#     Name = "S3 Remote Terraform State Store"
-#   }
-# }
+resource "aws_s3_bucket" "terraform_states_backend" {
+  bucket = var.backend_bucket_name
+  tags = {
+    Name = "S3 Remote Terraform State Store"
+  }
+}
 
-# resource "aws_s3_bucket_ownership_controls" "terraform_states_backend" {
-#   bucket = aws_s3_bucket.terraform_states_backend.id
-#   rule {
-#     object_ownership = "BucketOwnerPreferred"
-#   }
-# }
+resource "aws_s3_bucket_ownership_controls" "terraform_states_backend" {
+  bucket = aws_s3_bucket.terraform_states_backend.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
 
-# resource "aws_s3_bucket_acl" "terraform_states_backend" {
-#   depends_on = [aws_s3_bucket_ownership_controls.terraform_states_backend]
-#   bucket     = aws_s3_bucket.terraform_states_backend.id
-#   acl        = "private"
-# }
+resource "aws_s3_bucket_acl" "terraform_states_backend" {
+  depends_on = [aws_s3_bucket_ownership_controls.terraform_states_backend]
+  bucket     = aws_s3_bucket.terraform_states_backend.id
+  acl        = "private"
+}
 
-# resource "aws_s3_bucket_versioning" "terraform_states_backend" {
-#   bucket = aws_s3_bucket.terraform_states_backend.id
-#   versioning_configuration {
-#     status = "Enabled"
-#   }
-# }
+resource "aws_s3_bucket_versioning" "terraform_states_backend" {
+  bucket = aws_s3_bucket.terraform_states_backend.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
 
-# resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_states_backend" {
-#   bucket = aws_s3_bucket.terraform_states_backend.id
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_states_backend" {
+  bucket = aws_s3_bucket.terraform_states_backend.id
 
-#   rule {
-#     apply_server_side_encryption_by_default {
-#       sse_algorithm = "AES256"
-#     }
-#   }
-# }
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
 
 resource "aws_dynamodb_table" "terraform_states_backend" {
   name           = "${var.backend_bucket_name}-lock"
