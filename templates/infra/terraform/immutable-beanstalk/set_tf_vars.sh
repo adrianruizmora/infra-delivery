fe458af55844b494d06b1c5ef214e9dc6c9d81fa
Adrian Ruiz <adrianruizmora@hotmail.com>
2023-06-01 10:58:24 +0200

    feat: adding default tf vars set in pipeline

--- templates/infra/terraform/immutable-beanstalk/set_tf_vars.sh
+++ templates/infra/terraform/immutable-beanstalk/set_tf_vars.sh
@@ -2,8 +2,8 @@
 
 export TF_VAR_backend_bucket_name=""
 export TF_VAR_aws_region=""
-export TF_VAR_application=""
-export TF_VAR_environment=""
+export TF_VAR_application="$EB_APP_NAME" # Set by pipeline
+export TF_VAR_environment="$ENVIRONMENT" # Set by pipeline
 export TF_VAR_solution_stack=""
 export TF_VAR_ssh_access_key=""
 export TF_VAR_healthcheck_endpoint=""
