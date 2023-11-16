d7155ed0e6bcb9ec83a0a733907477f6852c7d26
Adrian Ruiz <adrianruizmora@hotmail.com>
2023-11-16 10:37:00 +0100

    refacto: terraform fmt

--- main.tf
+++ main.tf
@@ -260,13 +260,13 @@ resource "aws_elastic_beanstalk_environment" "compute" {
 # If the ID of existing security group it's not given
 # The creation of this ressource will be skipped.
 resource "aws_security_group_rule" "AllowEFSAccess" {
-  count = var.security_group_with_efs_access != "" ? 1 : 0
-  security_group_id = var.security_group_with_efs_access
-  description = "${var.application}-${var.environment}"
-  type        = "ingress"
-  from_port   = 2049
-  to_port     = 2049
-  protocol    = "tcp"
+  count                    = var.security_group_with_efs_access != "" ? 1 : 0
+  security_group_id        = var.security_group_with_efs_access
+  description              = "${var.application}-${var.environment}"
+  type                     = "ingress"
+  from_port                = 2049
+  to_port                  = 2049
+  protocol                 = "tcp"
   source_security_group_id = aws_elastic_beanstalk_environment.compute.autoscaling_security_groups
 }
 
