b3bf4cb9ae72007de478c1aa2c943980d749f37c
Adrian Ruiz <adrianruizmora@hotmail.com>
2023-11-17 12:39:05 +0100

    test: output for eb autoscaling groups

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "eb_app_cname" {
   value = aws_elastic_beanstalk_environment.compute.cname
 }
+
+output "eb_autoscaling_groups" {
+  value = aws_elastic_beanstalk_environment.compute.autoscaling_groups
+}
