f2eaee37263f03a787d7cbe36a027e824c64e0df
Adrian Ruiz <adrianruizmora@hotmail.com>
2023-11-17 11:28:31 +0100

    fix: adding id attribute to autoscaling security group

--- main.tf
+++ main.tf
@@ -281,7 +281,7 @@ resource "aws_security_group_rule" "AllowEFSAccess" {
   from_port                = 2049
   to_port                  = 2049
   protocol                 = "tcp"
-  source_security_group_id = aws_elastic_beanstalk_environment.compute.autoscaling_security_groups.id
+  source_security_group_id = aws_elastic_beanstalk_environment.compute.autoscaling_groups.id
 }
 
 resource "cloudflare_record" "compute" {
