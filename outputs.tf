de1f216b6ec63addc5ed68f107731a1519a667b0
Adrian Ruiz <adrianruizmora@hotmail.com>
2023-11-17 12:43:53 +0100

    test: add dependcy only

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,3 @@
 output "eb_app_cname" {
   value = aws_elastic_beanstalk_environment.compute.cname
 }
-
-output "eb_autoscaling_groups" {
-  value = aws_elastic_beanstalk_environment.compute.autoscaling_groups
-}
